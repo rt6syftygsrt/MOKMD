d11507a1a34ffd82b9a155611e6e8e695c62803f
saaid35 <46090530+rt6syftygsrt@users.noreply.github.com>
2019-01-30 09:01:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,59 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
-client.on('ready', () => {
-console.log(Logged in as ${client.user.tag}!);
-client.user.setGame(Nothing,"http://twitch.tv/S-F%22)
-console.log('')
-console.log('')
-console.log('╔[════════════════════════════════════
-console.log([Start] ${new Date()});
-console.log('╚[════════════════════════════════════
-console.log('')
-console.log('╔[════════════════════════════════════]
-console.log(Logged in as * [ " ${client.user.username} " ]);
-console.log('')
-console.log('Informations :')
-console.log('')
-console.log(servers! [ " ${client.guilds.size} " ]);
-console.log(Users! [ " ${client.users.size} " ]);
-console.log(channels! [ " ${client.channels.size} " ]);
-console.log('╚[════════════════════════════════════]
-console.log('')
-console.log('╔[════════════]╗')
-console.log(' Bot Is Online')
-console.log('╚[════════════]╝')
-console.log('')
-console.log('')
-});
+
+
+
+
 client.login('NTM5ODMxNTQwMzY5MzI2MTEw.DzLNQw.4XEYsm85UklrWL0VfrHgUSxhb1c'); 
+
+
+client.on('ready',  () => {
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
+  console.log('by safa7');
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  console.log(`Logged in as  * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
+  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+  console.log('is online')
+client.user.setStatus("dnd");
+});
+
+// playing
+client.on('ready', () => {                           
+client.user.setGame(``,'https://www.twitch.tv/tarikrs');                                                                                                                                                                                                                                                                                                                                                                                                                             client.user.setGame(`-help | On 20 Servers`,'https://www.twitch.tv/v5bz');
+});          
+
+
+
+         
+
+client.on('تثبيت', () => {
+var x = client.channels.get("504807381373419541");
+if (x) x.join();
+});
+
+
+
+
+client.on("message", (message) => {
+                        if (message.content.startsWith("ch")) {
+                                    if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
+                                let args = message.content.split(" ").slice(1);
+                            message.guild.createChannel(args.join(' '), 'text');
+                            message.channel.sendMessage('تـم إنـشاء روم كتابي')
+                            
+                        }
+                        });
+
+
+
+client.on("message", (message) => {
+                        if (message.content.startsWith("cv")) {
+                                    if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
+                                let args = message.content.split(" ").slice(1);
+                            message.guild.createChannel(args.join(' '), 'voice');
+                            message.channel.sendMessage('تـم إنـشاء روم صـوتي')
+                            
+                        }
+                        });
+
+
